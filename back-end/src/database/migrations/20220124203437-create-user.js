6fd605c15a97d8883fd60c365512d8100ceba31f
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-24 18:04:12 -0300

    returning user model user

--- back-end/src/database/migrations/20220124203437-create-user.js
+++ back-end/src/database/migrations/20220124203437-create-user.js
@@ -1,32 +1,32 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('users', {
+    await queryInterface.createTable("users", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       email: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       password: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       role: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('users');
-  }
+    await queryInterface.dropTable("users");
+  },
 };
--- back-end/src/database/models/user.js
+++ back-end/src/database/models/user.js
@@ -1,16 +1,17 @@
-
 module.exports = (sequelize, DataTypes) => {
-
   const User = sequelize.define(
-    'user',
+    "user",
     {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.STRING,
-  }, {
-    timestamps: false,
-    tableName: 'users',
-  },
-);
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      role: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      tableName: "users",
+    }
+  );
+
+  return User;
 };
