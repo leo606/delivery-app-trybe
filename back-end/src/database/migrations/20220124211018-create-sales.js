80f1254231ba98c83921ba15a05a22856dec91b5
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-24 18:10:59 -0300

    run model create sales

---
+++ back-end/src/database/migrations/20220124211018-create-sales.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('sales', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user: {
+        type: Sequelize.INTEGER
+      },
+      seller: {
+        type: Sequelize.INTEGER
+      },
+      total_price: {
+        type: Sequelize.DECIMAL
+      },
+      delivery_address: {
+        type: Sequelize.STRING
+      },
+      delivery_number: {
+        type: Sequelize.STRING
+      },
+      sale_date: {
+        type: Sequelize.DATE
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('sales');
+  }
+};
