a096e505909b0165212b6008888803342f264357
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-25 16:47:37 -0300

    status codes

--- back-end/src/api/middlewares/errors/error.js
+++ back-end/src/api/middlewares/errors/error.js
@@ -1,6 +1,5 @@
-const error = (err, _req, res, _next) => {
-  console.log('err', err);
-  return res.status(err.code).json({ message: err.message });
-};
+const statusCodes = require('../../../helpers/statusCodes.json');
 
+const error = (err, _req, res, _next) => 
+   res.status(statusCodes[err.code]).json({ message: err.message });
 module.exports = error;
