8dcc37a59fee5faf3940e8e8b404b5476881828e
Tulio <tuliotrindade99@gmail.com>
2022-01-25 16:54:17 -0300

    inicio registro back end

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const error = require('./middlewares/erros/error');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(require('../controller'));
 
--- back-end/src/controller/register/create.js
+++ back-end/src/controller/register/create.js
@@ -1,5 +1,17 @@
-module.exports = async (_req, res, _next) => {
+const crypto = require('crypto');
+const userServices = require('../../services/user/createUser')
+
+module.exports = async (req, res, _next) => {
   try {
+    const { password, name, email } = req.body;
+    const encryptedPassword = crypto.createHash('md5').update(password).digest("hex");
+    const data = {
+      name,
+      email,
+      encryptedPassword,
+    }
+    console.log(data)
+    await userServices.create(data)
     res.status(501).end();
   } catch (e) {
     console.log(e);
