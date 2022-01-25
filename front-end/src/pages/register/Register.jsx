13e98bbecc8886d19d8b8d54954bb2d586575332
0xguidev <guilhermeviniciustrabalho@gmail.com>
2022-01-25 19:18:45 -0300

    post request

--- front-end/src/pages/register/Register.jsx
+++ front-end/src/pages/register/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
-import { NEW_REGISTER } from '../../redux/reducers/register';
+import { NEW_REGISTER, POST_REGISTER } from '../../redux/reducers/register';
 import Input from '../../components/input';
 
 function Register() {
@@ -19,55 +20,62 @@ function Register() {
 
   function newRegister() {
     dispatch({ type: NEW_REGISTER, state: { name: fullName, email, password } });
-    dispatch({type: POST_REGISTER, return: async() => {
-      return await axios.post("https://yourapihere.com/path", {
-        
-      })
-    }})
-}
+  }
 
-function handleSubmit(e) {
-  e.preventDefault();
-  newRegister();
-}
+  async function postRegister() {
+    dispatch({ type: POST_REGISTER });
+    const result = async () => axios.post('http://localhost:3001/register', {
+      userData,
+    });
 
-return (
-  <div>
-    <form onSubmit={handleSubmit}>
-      <Input
-        type="text"
-        testId="common_register__input-name"
-        placeholder="Seu nome"
-        value={fullName}
-        name="name"
-        onChange={handleChange}
-      />
-      <Input
-        type="email"
-        testId="common_register__input-email"
-        placeholder="email@tryber.com.br"
-        value={email}
-        name="email"
-        onChange={handleChange}
-      />
-      <Input
-        type="password"
-        testId="common_register__input-password"
-        placeholder="********"
-        value={password}
-        name="password"
-        onChange={handleChange}
-      />
-      <button type="submit" data-testid="common_register__button-register">
-        REGISTER
-      </button>
-      <span data-testid="common_register__element-invalid_register ">
-        Dados invalidios
-      </span>
-      <button type="button" onClick={() => console.log(userData)}>redux</button>
-    </form>
-  </div>
-);
+    console.log(result());
+    // {type: API_SUCESS}
+    // {type: API_ERROR}
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    newRegister();
+    postRegister();
+  }
+
+  return (
+    <div>
+      <form onSubmit={ handleSubmit }>
+        <Input
+          type="text"
+          testId="common_register__input-name"
+          placeholder="Seu nome"
+          value={ fullName }
+          name="name"
+          onChange={ handleChange }
+        />
+        <Input
+          type="email"
+          testId="common_register__input-email"
+          placeholder="email@tryber.com.br"
+          value={ email }
+          name="email"
+          onChange={ handleChange }
+        />
+        <Input
+          type="password"
+          testId="common_register__input-password"
+          placeholder="********"
+          value={ password }
+          name="password"
+          onChange={ handleChange }
+        />
+        <button type="submit" data-testid="common_register__button-register">
+          REGISTER
+        </button>
+        <span data-testid="common_register__element-invalid_register ">
+          Dados invalidios
+        </span>
+        <button type="button" onClick={ () => console.log(userData) }>redux</button>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
--- front-end/src/redux/reducers/register.js
+++ front-end/src/redux/reducers/register.js
@@ -1,15 +1,35 @@
 export const NEW_REGISTER = 'NEW_REGISTER';
+export const POST_REGISTER = 'POST_REGISTER';
+export const API_SUCESS = 'API_SUCESS';
+export const API_ERROR = 'API_ERROR';
 
-const INITIAL_STATE = {
+const initialState = {
   name: '',
   email: '',
   password: '',
+  token: '',
+  isLoading: false,
 };
 
-function registerReducer(state = INITIAL_STATE, action) {
+function registerReducer(state = initialState, action) {
   switch (action.type) {
   case NEW_REGISTER:
     return { state: action.state };
+  case POST_REGISTER:
+    return { ...state, isLoading: true };
+  case API_SUCESS:
+    return {
+      ...state,
+      error: null,
+      token: action.payload.results,
+      isLoading: false,
+    };
+  case API_ERROR:
+    return {
+      ...state,
+      error: action.eror,
+      isLoading: false,
+    };
   default:
     return state;
   }
