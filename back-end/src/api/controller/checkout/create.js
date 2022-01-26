35f2ac5cd669ac37e74f6533ebfe1707690fb501
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-26 19:26:31 -0300

    checkout service

--- back-end/src/api/controller/checkout/create.js
+++ back-end/src/api/controller/checkout/create.js
@@ -5,3 +5,10 @@ module.exports = async (_req, res, _next) => {
     console.log(e);
   }
 };
+
+// sale from front:
+// {
+//   sellerId,
+//   products: [{ id, quantity }, ...],
+//   total,
+// }
--- back-end/src/services/checkout/create.js
+++ back-end/src/services/checkout/create.js
@@ -1,10 +1,44 @@
+const Sequelize = require('sequelize');
 const { sales } = require('../../database/models');
 
-module.exports = async (sale) => {
-  try {
-    const create = await sales.create(sale);
-    return create;
-  } catch (e) {
-    console.log(e);
-  }
-};
+const environment = process.env.NODE_ENV || 'test';
+const sequelizeConfig = require('../../database/config/config');
+
+const sequelize = new Sequelize(sequelizeConfig[environment]);
+
+// module.exports = async (sale) => {
+//   try {
+//     const transaction = await sequelize.transaction(async () => {
+//       // create sale
+//       const createSale = await sales.create
+
+//       // add sales-products
+//     });
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+// sale from front:
+// {
+//   sellerId,
+//   products: [{ id, quantity }, ...],
+//   total,
+// }
+
+// sale:
+// {
+//   user_id: 2,
+//   seller_id: 1,
+//   total_price: 99.9,
+//   delivery_address: "address",
+//   delivery_number: "23",
+//   sale_date: new Date(Date.now()),
+//   status: "ordered",
+// },
+
+// product:
+// {
+//   name: "Becks 600ml",
+//   price: 8.89,
+//   url_image: "http://localhost:3001/images/beer.jpg",
+// },
