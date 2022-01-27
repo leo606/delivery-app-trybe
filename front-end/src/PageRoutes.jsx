af96b1a04821115932a803371475872700979263
RyanLaiber <ryanlaiber@gmail.com>
2022-01-27 15:17:33 -0300

    adiciona rotas de checkout, pedidos

--- front-end/src/PageRoutes.jsx
+++ front-end/src/PageRoutes.jsx
@@ -3,12 +3,16 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Products from './pages/products/Products';
 import Register from './pages/register/Register';
+import Orders from './pages/orders/Orders';
+import Checkout from './pages/checkout/Checkout';
 
 function PageRoutes() {
   return (
     <Routes>
       <Route exact path="/login" element={ <Login /> } />
       <Route exact path="/customer/products" element={ <Products /> } />
+      <Route exact path="/customer/orders" element={ <Orders /> } />
+      <Route exact path="/customer/checkout" element={ <Checkout /> } />
       <Route exact path="/register" element={ <Register /> } />
       <Route exact path="/" element={ <Navigate to="/login" /> } />
     </Routes>
