8938b505b7a99aa1d9c7ac02009f12d9214115a5
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-27 17:39:21 -0300

    checkout product table

---
+++ front-end/src/helpers/calcTotal.js
@@ -0,0 +1,11 @@
+// import formatCurrency from './formatCurrency';
+
+// export default function calcTotal(value) {
+//   let total = 0;
+
+//   cart.forEach((e) => {
+//     const { price, quantity } = e;
+//     total += (price * quantity);
+//   });
+//   return total;
+// }
--- front-end/src/pages/checkout/ProductTr.jsx
+++ front-end/src/pages/checkout/ProductTr.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+
+import { deleteProduct } from '../../redux/actions/cart/deleteProduct';
+import formatCurrency from '../../helpers/formatCurrency';
 
 function ProductTr({ product }) {
+  const dispatch = useDispatch();
+
   return (
     <tr key={ product.id }>
       <td>{product.id}</td>
+      <td>{product.name}</td>
+      <td>{product.quantity}</td>
+      <td>{product.price}</td>
+      <td>{formatCurrency(product.quantity * product.price)}</td>
+      <td>
+        <button
+          data-testid={ `customer_checkout__element-order-table-remove-${product.id}` }
+          type="button"
+          onClick={ () => dispatch(deleteProduct({ id: product.id })) }
+        >
+          Remover
+        </button>
+      </td>
     </tr>
   );
 }
@@ -15,6 +34,7 @@ ProductTr.propTypes = {
     image: PropTypes.string,
     price: PropTypes.string,
     id: PropTypes.number,
+    quantity: PropTypes.number,
   }).isRequired,
 };
 
