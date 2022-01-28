59cc5ba4c5346a6fe8213cae985793e8128073ba
RyanLaiber <ryanlaiber@gmail.com>
2022-01-28 17:09:03 -0300

    implementa tela de registro de usuarios

--- back-end/src/api/controller/register/createAdmin.js
+++ back-end/src/api/controller/register/createAdmin.js
@@ -1,5 +1,5 @@
 const statusCodes = require('../../../helpers/statusCodes.json');
-const { createUser } = require('../../../services/register');
+const { create } = require('../../../services/users');
 
 module.exports = async (req, res, _next) => {
   const { user, body } = req;
@@ -8,6 +8,6 @@ module.exports = async (req, res, _next) => {
     return res.status(statusCodes.unauthorized).json({ message: 'unauthorized' });
   }
 
-  const { id } = await createUser({ name, email, password, role });
+  const { id } = await create({ name, email, password, role });
   return res.status(statusCodes.created).json({ id, name, email, role });
 };
