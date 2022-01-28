b8ec60dd61bda816b1c32e43770d65e382443e7f
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-28 10:43:09 -0300

    create user service

---
+++ back-end/src/services/users/getByEmail.js
@@ -0,0 +1,15 @@
+const model = require('../../database/models');
+
+module.exports = async (email) => {
+  try {
+    const user = await model.users.findOne({
+      where: { email },
+    });
+    if (!user) {
+      return { err: { code: 'notFound', message: 'user not found' } };
+    }
+    return user.dataValues;
+  } catch (e) {
+    console.log(e);
+  }
+};
