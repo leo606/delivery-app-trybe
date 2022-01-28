337ff98ee669c15d1175e38c32ed8d2f9e0f8b30
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-28 12:20:31 -0300

    migrations price type fix

--- back-end/src/database/migrations/20220124212107-create-products.js
+++ back-end/src/database/migrations/20220124212107-create-products.js
@@ -12,7 +12,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       price: {
-        type: Sequelize.DECIMAL(12, 2)
+        type: Sequelize.DECIMAL(10, 2)
       },
       url_image: {
         type: Sequelize.STRING
