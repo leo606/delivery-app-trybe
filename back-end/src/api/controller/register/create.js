aefdc5cfeb47baf09b81196f29f12beafe7220bf
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-28 10:39:28 -0300

    change register router signing register token in create middleware

--- back-end/src/api/controller/register/create.js
+++ back-end/src/api/controller/register/create.js
@@ -1,10 +1,10 @@
 const statusCodes = require('../../../helpers/statusCodes.json');
 const { createUser } = require('../../../services/register');
+const signJwt = require('../../../helpers/signJwt');
 
 module.exports = async (req, res, _next) => {
   try {
     const { password, name, email } = req.body;
-    const { token } = req.token;
     const role = 'customer';
     const data = {
       name,
@@ -13,6 +13,7 @@ module.exports = async (req, res, _next) => {
       role,
     };
     await createUser(data);
+    const token = signJwt(data);
     res.status(statusCodes.created).json({ name, email, role, token });
   } catch (e) {
     console.log(e);
