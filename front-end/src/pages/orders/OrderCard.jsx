32659cbd4970f5e92f530f3fe1a0ee02427154f5
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-29 14:23:51 -0300

    orders card

---
+++ front-end/src/pages/orders/OrderCard.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import formatSaleId from '../../helpers/formatSaleId';
+import formatCurrency from '../../helpers/formatCurrency';
+
+function OrderCard({ order }) {
+  const { id, totalPrice, saleDate, status } = order;
+
+  return (
+    <Link to={ `${id}` }>
+      <div>
+        <div>
+          <p>Pedido</p>
+          <p data-testid={ `customer_orders__element-order-id-${id}` }>
+            {formatSaleId(id)}
+          </p>
+        </div>
+        <div data-testid={ `customer_orders__element-delivery-status-${id}` }>
+          {status}
+        </div>
+        <div data-testid={ `customer_orders__element-order-date-${id}` }>
+          {new Date(saleDate).toLocaleDateString('pt-BR')}
+        </div>
+        <div>{formatCurrency(totalPrice)}</div>
+      </div>
+    </Link>
+  );
+}
+
+OrderCard.propTypes = {
+  order: PropTypes.shape({
+    id: PropTypes.number,
+    userId: PropTypes.number,
+    totalPrice: PropTypes.string,
+    deliveryAddress: PropTypes.string,
+    deliveryNumber: PropTypes.string,
+    saleDate: PropTypes.string,
+    status: PropTypes.string,
+  }).isRequired,
+};
+
+export default OrderCard;
--- front-end/src/pages/orders/Orders.jsx
+++ front-end/src/pages/orders/Orders.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from '../../components/header/Header';
 
+import getLocalStorage from '../../helpers/getLocalStorage';
+import OrderCard from './OrderCard';
+
+const ORDERS_LIST_URL = 'http://localhost:3001/customer/orders';
+
 function Orders() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    async function fetchOrders() {
+      try {
+        const config = {
+          headers: { authorization: getLocalStorage('user').token },
+        };
+        const res = await axios.get(ORDERS_LIST_URL, config);
+        setOrders(res.data);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    fetchOrders();
+  }, []);
+
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <main>
+        {orders.map((o) => (<OrderCard key={ o.id } order={ o } />))}
+      </main>
+    </>
   );
 }
 
