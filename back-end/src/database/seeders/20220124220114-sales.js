e7e7e3c3fe85ee7df7797424eb7c25556747bf28
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-01 11:03:04 -0300

    revert seeds

---
+++ back-end/src/database/seeders/20220124220114-sales.js
@@ -0,0 +1,52 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "sales",
+      [
+        {
+          user_id: 2,
+          seller_id: 2,
+          total_price: 99.9,
+          delivery_address: "address",
+          delivery_number: "23",
+          sale_date: new Date(Date.now()),
+          status: "Pendente",
+        },
+        {
+          user_id: 2,
+          seller_id: 2,
+          total_price: 99.9,
+          delivery_address: "address two",
+          delivery_number: "345",
+          sale_date: new Date(Date.now()),
+          status: "Preparando",
+        },
+        {
+          user_id: 2,
+          seller_id: 2,
+          total_price: 99.9,
+          delivery_address: "address three",
+          delivery_number: "7",
+          sale_date: new Date(Date.now()),
+          status: "Em Trânsito",
+        },
+        {
+          user_id: 2,
+          seller_id: 2,
+          total_price: 99.9,
+          delivery_address: "address four",
+          delivery_number: "87",
+          sale_date: new Date(Date.now()),
+          status: "Entregue",
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("sales", null, {});
+  },
+};
