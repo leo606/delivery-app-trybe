cf81d06f45c986d2cc77315ceece6ff9892b5418
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-07 17:21:25 -0300

    remove console log

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,16 +1,14 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || "test";
-console.log(99999, environment);
+// const environment = process.env.NODE_ENV || "test";
 
-const suffix = {
-  prod: "",
-  production: "",
-  dev: "-dev",
-  development: "-dev",
-  test: "-test",
-};
-console.log(888, suffix[environment]);
+// const suffix = {
+//   prod: "",
+//   production: "",
+//   dev: "-dev",
+//   development: "-dev",
+//   test: "-test",
+// };
 
 const options = {
   host: process.env.MYSQL_HOST || 'mysql',
@@ -24,8 +22,6 @@ const options = {
   },
   logging: false,
 };
-
-console.log(10101, options);
 
 module.exports = {
   development: {
